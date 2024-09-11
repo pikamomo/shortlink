002949d2014edc0945d5f6cf2a48d80dd2800f38
NoElectricityPika <okchizhang@gmail.com>
2024-09-11 16:49:28 -0400

    2024/9/11 add global exception handler

--- admin/src/main/java/com/chi/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/chi/shortlink/admin/controller/UserController.java
@@ -2,7 +2,6 @@ package com.chi.shortlink.admin.controller;
 
 import com.chi.shortlink.admin.common.convention.result.Result;
 import com.chi.shortlink.admin.common.convention.result.Results;
-import com.chi.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.chi.shortlink.admin.dto.resp.UserRespDTO;
 import com.chi.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -24,11 +23,6 @@ public class UserController {
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable String username) {
-        UserRespDTO result = userService.getUserByUsername(username);
-        if (result == null) {
-            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
-        } else {
-            return Results.success(result);
-        }
+        return Results.success(userService.getUserByUsername(username));
     }
 }
