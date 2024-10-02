50c44d986f3f0e92c6a450eb61a9037cb87e7ef1
NoElectricityPika <okchizhang@gmail.com>
2024-10-01 23:22:00 -0400

    feature: shout link redirect function

--- admin/src/main/java/com/chi/shortlink/admin/remote/dto/req/ShortLinkCreateReqDTO.java
+++ admin/src/main/java/com/chi/shortlink/admin/remote/dto/req/ShortLinkCreateReqDTO.java
@@ -12,10 +12,6 @@ import java.util.Date;
 public class ShortLinkCreateReqDTO {
 
     /**
-     * protocol type
-     */
-    private String domainProtocol;
-    /**
      * domain
      */
     private String domain;
