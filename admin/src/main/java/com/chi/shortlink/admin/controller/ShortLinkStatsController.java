3eb3a80b45a4c3fa9542d9163c59685abbd4c85b
chi002081804 <zhang.chi27@northeastern.edu>
2024-10-11 00:04:42 -0400

    feature: develop single short link data query function

--- admin/src/main/java/com/chi/shortlink/admin/controller/ShortLinkStatsController.java
+++ admin/src/main/java/com/chi/shortlink/admin/controller/ShortLinkStatsController.java
@@ -16,7 +16,7 @@ public class ShortLinkStatsController {
     };
 
     /**
-     * get short link stats
+     * get short link stat
      */
     @GetMapping("/api/short-link/admin/v1/stats")
     public Result<ShortLinkStatsRespDTO> shortLinkStats(ShortLinkStatsReqDTO requestParam) {
