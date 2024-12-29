752328a6e737a389bb91456802cc5339aa276ebe
chi002081804 <zhang.chi27@northeastern.edu>
2024-12-28 23:29:11 -0500

    optimize: add some comment

--- admin/src/main/java/com/chi/shortlink/admin/common/constant/RedisCacheConstant.java
+++
@@ -1,9 +0,0 @@
-package com.chi.shortlink.admin.common.constant;
-
-/**
- * short link admin system redis cache constant class
- */
-public class RedisCacheConstant {
-
-    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
-}
--- admin/src/main/java/com/chi/shortlink/admin/dto/req/UserLoginReqDTO.java
+++
@@ -1,9 +0,0 @@
-package com.chi.shortlink.admin.dto.req;
-
-import lombok.Data;
-
-@Data
-public class UserLoginReqDTO {
-    private String username;
-    private String password;
-}
